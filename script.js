20fde559f1d15ca08fb31437328a2cab2acb03f9
SBS Studio © <140276769+sbsgif@users.noreply.github.com>
2024-02-23 22:16:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function toggleTheme() {
-    if (document.body.classList.contains("dark"))
-        document.body.classList.remove("dark");
-    else
-        document.body.classList.add("dark");
-}
+
